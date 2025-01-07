32acd98b5010217a0fa35b687e588a165440ee0c
Lucas Quadros <1lucascq@gmail.com>
2025-01-07 11:46:40 -0300

    feat: update main.ts to include seed script

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { SeedService } from './seed/seed.service';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+    const app = await NestFactory.create(AppModule);
+    app.useGlobalPipes(new ValidationPipe());
+
+    const seedService = app.get(SeedService);
+    if (process.argv.includes('--seed')) {
+        await seedService.runSeed();
+        process.exit();
+    }
+
+    await app.listen(3003);
 }
 bootstrap();
