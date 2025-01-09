4e582b73808ec90acb71e2072cdc1f91de5c91f3
Lucas Quadros <1lucascq@gmail.com>
2025-01-09 17:33:44 -0300

    feat: refactor app module and controller for improved structure and add global interceptor

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -6,12 +6,14 @@ import { CategoryModule } from './category/category.module';
 import { OrderModule } from './order/order.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { SeedModule } from './seed/seed.module';
+import { DashboardModule } from './dashboard/dashboard.module';
 
-const URL = 'mongodb://localhost:27017/hubxp_demo';
+
+const MONGODB_URL = process.env.MONGODB_URL || 'mongodb://localhost:27017/hubxp_demo?directConnection=true&replicaSet=rs0';
 
 @Module({
-  imports: [ProductModule, CategoryModule, OrderModule, MongooseModule.forRoot(URL), SeedModule],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [ProductModule, CategoryModule, OrderModule, MongooseModule.forRoot(MONGODB_URL), SeedModule, DashboardModule],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}
