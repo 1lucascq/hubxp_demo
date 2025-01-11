c0f8e69de94000d24c33f1aef50a3fd1244413a3
Lucas Quadros <1lucascq@gmail.com>
2025-01-11 14:11:06 -0300

    feat: enable CORS for the application with specified methods and credentials

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -5,6 +5,13 @@ import { SeedService } from './seed/seed.service';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+
+    app.enableCors({
+        origin: true,
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+        credentials: true,
+    });
+
     app.useGlobalPipes(new ValidationPipe());
     app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
 
